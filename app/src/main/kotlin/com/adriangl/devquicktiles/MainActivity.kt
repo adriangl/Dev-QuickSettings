cf2eff7001fb78382580c1040aff9da323a448c7
Adrián García <adriangarcia.lopez@gmail.com>
2017-04-20 17:13:39 +0200

    Added nav drawer; WIP

--- app/src/main/kotlin/com/adriangl/devquicktiles/MainActivity.kt
+++ app/src/main/kotlin/com/adriangl/devquicktiles/MainActivity.kt
@@ -18,36 +18,56 @@ package com.adriangl.devquicktiles
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.Menu
+import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.adriangl.devquicktiles.tiles.views.QsDescriptionsItem
 import com.adriangl.devquicktiles.tiles.views.QsDescriptionsRecyclerViewAdapter
 import com.adriangl.devquicktiles.utils.PermissionUtils
 import com.mikepenz.aboutlibraries.Libs
 import com.mikepenz.aboutlibraries.LibsBuilder
 
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-class MainActivity : AppCompatActivity() {
-
+    @BindView(R.id.qs_tile_descriptions_recyclerview)
     lateinit var qsDescriptionsRecyclerView: RecyclerView
+    @BindView(R.id.grant_permissions_container)
     lateinit var grantPermissionsView: View
+    @BindView(R.id.grant_permissions_text)
     lateinit var grantPermissionsText: TextView
+    @BindView(R.id.permissions_share)
     lateinit var permissionsShareButton: Button
+
+    private lateinit var drawer : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        qsDescriptionsRecyclerView = findViewById(R.id.qs_tile_descriptions_recyclerview) as RecyclerView
-        grantPermissionsView = findViewById(R.id.grant_permissions_container)
-        grantPermissionsText = findViewById(R.id.grant_permissions_text) as TextView
-        permissionsShareButton = findViewById(R.id.permissions_share) as Button
+        ButterKnife.bind(this)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)
 
         permissionsShareButton.setOnClickListener { sharePermissions() }
     }
@@ -68,30 +88,26 @@ class MainActivity : AppCompatActivity() {
         if (hasNeededPermissions()) grantPermissionsView.visibility = View.GONE
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.activity_main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when {
-            item?.itemId == R.id.menu_about -> showAbout()
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
-        return true
     }
 
-    private fun showAbout() {
-        LibsBuilder()
-                .withFields(R.string::class.java.fields)
-                .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
-                .withActivityTitle(getString(R.string.menu_about))
-                .withAboutIconShown(true)
-                .withAboutAppName(getString(R.string.app_name))
-                .withAboutVersionShownName(true)
-                .withAboutVersionShownCode(false)
-                .withAutoDetect(true)
-                .withExcludedLibraries("AndroidIconics", "fastadapter")
-                .start(this)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        when (id) {
+            R.id.nav_settings -> showSettings()
+            R.id.nav_about -> showAbout()
+        }
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 
     private fun getDescriptionItems(): List<QsDescriptionsItem> {
@@ -126,7 +142,7 @@ class MainActivity : AppCompatActivity() {
     private fun getFormattedPermissions() = getString(R.string.permissions_to_grant, BuildConfig.APPLICATION_ID)
 
     private fun sharePermissions() {
-        val sharingIntent = Intent(android.content.Intent.ACTION_SEND)
+        val sharingIntent = Intent(Intent.ACTION_SEND)
         sharingIntent.type = "text/plain"
         sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
                 getString(R.string.share_permissions_subject, getString(R.string.app_name)))
@@ -137,5 +153,23 @@ class MainActivity : AppCompatActivity() {
     private fun hasNeededPermissions(): Boolean {
         return (PermissionUtils.checkSelfPermissionIsGranted(this, android.Manifest.permission.WRITE_SECURE_SETTINGS)
                 and (PermissionUtils.checkSelfPermissionIsGranted(this, android.Manifest.permission.DUMP)))
+    }
+
+    private fun showSettings() {
+
+    }
+
+    private fun showAbout() {
+        LibsBuilder()
+                .withFields(R.string::class.java.fields)
+                .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
+                .withActivityTitle(getString(R.string.menu_about))
+                .withAboutIconShown(true)
+                .withAboutAppName(getString(R.string.app_name))
+                .withAboutVersionShownName(true)
+                .withAboutVersionShownCode(false)
+                .withAutoDetect(true)
+                .withExcludedLibraries("AndroidIconics", "fastadapter")
+                .start(this)
     }
 }
